f8bf4619812d39f5ae6084718180a964333a6d84
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2024-04-19 17:57:23 -0400

    Added some more logic in App.js Component in particular I added state and an event handler, I also added some more logic to the TodoCreate component added state 2 event handlers and a form element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import TodoCreate from "./components/TodoCreate";
+import { useState } from "react";
+import TodoCreate from './components/TodoCreate';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = (todoItem) => {
+    console.log('You need to:', todoItem);
+  }
+
   return(
     <div>
-      <TodoCreate/>
+      <TodoCreate onCreate = {createTodo} />
     </div>
   )
 }
 
 export default App;
+
+/* We can represent each todo item with an object that has id property and a todoItem property, and then 
+for every object that gets created we can render a different instance of the todoshow property
+... we can store every single object that gets created inside of an array so that we have 
+one single data structure that keeps track of all the books that have been created throughout
+time. Being that we would want the user to update the content on the screen we would use 
+the state system.
+*/
--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,7 +1,27 @@
-function TodoCreate() {
+import { useState } from "react";
+
+function TodoCreate({onCreate}) {
+  
+  const [todoItem, setTodoItem] = useState('');
+
+  const handleChange = (event) => {
+    setTodoItem(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(todoItem);
+    setTodoItem('');
+  }
+
+
   return(
     <div>
-      TodoCreate
+      <form onSubmit={handleSubmit}>
+        <label>Todo Item</label>
+        <input value = {todoItem} onChange = {handleChange} />
+        <button>ADD</button>
+      </form>
     </div>
   )
 }
